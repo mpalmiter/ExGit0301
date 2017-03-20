749c9dd97344b7d13ce22c7cb1fe60eaa194aee1
Maggie Palmiter <mpalmiter7673@vbtc.net>
2017-03-20 13:24:21 -0400

    initial commit

---
+++ Git0301/Git0301/Git0301.cpp
@@ -0,0 +1,15 @@
+// Git0301.cpp : Defines the entry point for the console application.
+//
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	cout << "Hello World!\n";
+	system("pause");
+	return 0;
+}
+
