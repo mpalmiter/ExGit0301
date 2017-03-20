3f7a511b091a3ec0c9c5cda1eccd3ff3cc8ff11a
Jacob Modelewski <jmodelewski7620@vbtc.net>
2017-03-20 13:56:06 -0400

    D2 Done

--- Git0301/Git0301/Git0301.cpp
+++ Git0301/Git0301/Git0301.cpp
@@ -13,3 +13,19 @@ int main()
 	return 0;
 }
 
+// return a jumbled version of aWord
+string mixUP(const string& aWord)
+{
+	string aJumble = aWord;
+	int length = static_cast<int>(aJumble.size());
+
+	for (int i = 0; i < length; ++i)
+	{
+		int randomIndex = (rand() % length);
+		char temp = aJumble[i];
+		aJumble[i] = aJumble[randomIndex];
+		aJumble[randomIndex] = temp;
+	}
+	return aJumble;
+}
+
