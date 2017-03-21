83185ac06a0014d2eb3fad39781d61109585598a
Maggie Palmiter <mpalmiter7673@vbtc.net>
2017-03-21 13:40:08 -0400

    adding cpp files

---
+++ Git0301/Git0301/Git0301.cpp
@@ -0,0 +1,45 @@
+// Git0301.cpp : Defines the entry point for the console application.
+//
+
+#include <string>
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+string randomWord();
+string mixUp(const string& aWord);
+void welcomePlayer(const string& aJumble);
+void play(const string&aWord);
+
+
+int main()
+{
+	srand(static_cast<unsigned int>(time(0)));
+
+	string word = randomWord();
+	/*string jumble = mixUp(word);
+	welcomePlayer(jumble);
+	play(word);*/
+
+	system("Pause");
+	return 0;
+}
+
+string randomWord()
+{
+	const int NUM_WORDS = 5;
+	const string WORDS[NUM_WORDS] =
+	{
+		"wall",
+		"glass",
+		"party",
+		"strawberry",
+		"animal",
+	};
+	
+	int choice = (rand() % NUM_WORDS);
+	string word = WORDS[choice];
+
+	return word;
+}
+
