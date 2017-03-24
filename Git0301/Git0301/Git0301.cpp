4e59c9b68f0b2834795661e2f8cf3369e4bbd6c3
Isma <isalinas0361@vbtc.net>
2017-03-24 14:21:10 -0400

    Hello

--- Git0301/Git0301/Git0301.cpp
+++ Git0301/Git0301/Git0301.cpp
@@ -48,9 +48,19 @@ string randomWord() // Maggie's
 	return word;
 }
 
-string mixUp(const string& aWord)
+string mixUp(const string& aWord) //Isma's
 {
-	return string();
+	string aJumble = aWord;
+	int length = static_cast<int>(aJumble.size());
+
+	for (int i = 0; i < length; ++i)
+	{
+		int randomIndex = (rand() % length);
+		char temp = aJumble[i];
+		aJumble[i] = aJumble[randomIndex];
+		aJumble[randomIndex] = temp;
+	}
+	return aJumble;
 }
 
 void welcomePlayer(const string& aJumble)
